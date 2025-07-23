272d41bffba17eee873947e6492a1e2bc2cff298
Robin Kaltenbach <robin@cskaltenbach.de>
2025-07-23 21:23:53 +0200

    override equals

--- src/NexGrades/NexGrades.Domain/Models/Subject.cs
+++ src/NexGrades/NexGrades.Domain/Models/Subject.cs
@@ -3,4 +3,16 @@
 public class Subject
 {
     public required string Name { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Subject sub)
+        {
+            return Name.Equals(sub.Name);
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
