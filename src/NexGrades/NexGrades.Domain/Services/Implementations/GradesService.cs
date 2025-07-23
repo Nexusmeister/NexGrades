51a7d1ee3a36b92a0fcb4c1961afc5805081479c
Robin Kaltenbach <robin@cskaltenbach.de>
2025-07-23 21:24:04 +0200

    implement gradesService

---
+++ src/NexGrades/NexGrades.Domain/Services/Implementations/GradesService.cs
@@ -0,0 +1,49 @@
+﻿using NexGrades.Domain.Models;
+using NexGrades.Domain.Services.Interfaces;
+
+namespace NexGrades.Domain.Services.Implementations;
+
+public sealed class GradesService : IGradesService
+{
+    public Grade CalculateAllGrades(Student student)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Grade CalculateGradeBySubject(Student student, Subject subject)
+    {
+        var relevantGrades = GetGradesFromStudent(student.Grades, subject).ToList();
+        var calculatedGrade = CalculateSubGrade(relevantGrades);
+
+        return new Grade
+        {
+            Multiplier = 1,
+            SubGrades = relevantGrades,
+            Subject = subject,
+            Value = calculatedGrade
+        };
+    }
+
+
+    private IEnumerable<Grade> GetGradesFromStudent(IEnumerable<Grade> gradesOfStudent, Subject subject)
+    {
+        var gradesToReturn = gradesOfStudent.Where(x => x.Subject.Equals(subject));
+        return gradesToReturn;
+    }
+
+    private static decimal CalculateSubGrade(IList<Grade> subGrades)
+    {
+        var sum = 0.00M;
+        foreach (var grade in subGrades)
+        {
+            if (grade.SubGrades.Any())
+            {
+                grade.Value = CalculateSubGrade(grade.SubGrades);
+            }
+
+            sum += grade.Value * grade.Multiplier;
+        }
+
+        return sum / subGrades.Count;
+    }
+}
---
+++ src/NexGrades/NexGrades.Domain/Services/Interfaces/IGradesService.cs
@@ -0,0 +1,9 @@
+﻿using NexGrades.Domain.Models;
+
+namespace NexGrades.Domain.Services.Interfaces;
+
+public interface IGradesService
+{
+    Grade CalculateAllGrades(Student student);
+    Grade CalculateGradeBySubject(Student student, Subject subject);
+}
