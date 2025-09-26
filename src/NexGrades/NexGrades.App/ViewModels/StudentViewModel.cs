4512976105b3ff7d4e3538e0d72686c35d3a475d
Robin Kaltenbach <robin@cskaltenbach.de>
2025-09-27 00:45:08 +0200

    add buttons for saving and canceling

--- src/NexGrades/NexGrades.App/ViewModels/StudentViewModel.cs
+++ src/NexGrades/NexGrades.App/ViewModels/StudentViewModel.cs
@@ -1,9 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using NexGrades.Domain.Models;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
 
 namespace NexGrades.App.ViewModels;
 
-public partial class StudentViewModel : ViewModel
+public partial class StudentViewModel(INavigationService navigation, ISnackbarService snackbar) : ViewModel
 {
     [ObservableProperty] private Student _student;
+    [ObservableProperty] private string _title = "AddStudent";
+
+    [RelayCommand]
+    private void SaveStudent()
+    {
+        snackbar.Show("Success", "Student has been saved", ControlAppearance.Success, new SymbolIcon(SymbolRegular.CheckmarkCircle32), TimeSpan.FromSeconds(5));
+        navigation.GoBack();
+    }
+
+    [RelayCommand]
+    private void Cancel()
+    {
+        navigation.GoBack();
+    }
 }
