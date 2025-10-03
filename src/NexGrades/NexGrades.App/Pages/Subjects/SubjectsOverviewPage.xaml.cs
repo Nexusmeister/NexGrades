74da4ad6dce6730307536907c7351d60b9a69c0b
Robin Kaltenbach <robin@cskaltenbach.de>
2025-10-03 17:46:30 +0200

    add pages

---
+++ src/NexGrades/NexGrades.App/Pages/Subjects/SubjectsOverviewPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NexGrades.App.Pages.Subjects
+{
+    /// <summary>
+    /// Interaction logic for SubjectsOverviewPage.xaml
+    /// </summary>
+    public partial class SubjectsOverviewPage : Page
+    {
+        public SubjectsOverviewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
